3350ffe1f78bdd20b8e9f5054c7687d3df39cfb2
BradLi <lixzzzzzz@126.com>
2019-11-08 10:20:25 +0800

    移除链表元素

---
+++ src/链表/_203_移除链表元素.java
@@ -0,0 +1,47 @@
+package 链表;
+
+public class _203_移除链表元素 {
+
+    public static ListNode removeElements(ListNode head, int val) {
+
+        ListNode node = head;
+        while (node!=null) {
+            if (node.val == val){
+                node.val = node.next.val;
+                node.next = node.next.next;
+                continue;
+            }
+            node = node.next;
+        }
+        return head;
+    }
+
+
+    public ListNode removeElements1(ListNode head, int val) {
+        ListNode header = new ListNode(-1);
+        header.next = head;
+        ListNode cur = header;
+        while(cur.next != null){
+            if(cur.next.val == val ){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return header.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(1);
+//        node.next.next = new ListNode(6);
+//        node.next.next.next = new ListNode(3);
+//        node.next.next.next.next = new ListNode(4);
+//        node.next.next.next.next.next = new ListNode(5);
+//        node.next.next.next.next.next.next = new ListNode(6);
+
+         removeElements(node,1);
+
+        System.out.printf("hhh");
+    }
+}
