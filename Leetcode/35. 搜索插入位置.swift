39096c3eeb5462cfdab06bc3ffd4f725edaec8af
lxzzzzzz <lixzzzzzz@126.com>
2020-07-17 10:51:37 +0800

    数据结构修改

--- Leetcode/35. 搜索插入位置.swift
+++ Leetcode/35. 搜索插入位置.swift
@@ -11,37 +11,52 @@ import Cocoa
 class _35: NSObject {
 
 }
+//func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+//    if target <= nums[0] {
+//        return 0
+//    }
+//
+//    let right = nums.count - 1
+//    if target == nums[right] {
+//        return right
+//    }
+//    if target > nums[right] {
+//        return right + 1
+//    }
+//
+//    return index4Insert(nums, target: target, left: 0, right: right)
+//}
+//
+//private func index4Insert(_ nums: [Int], target: Int, left:Int,right:Int) -> Int{
+//
+//    if target == nums[right] {
+//        return right
+//    }
+//    if target == nums[left] {
+//        return left
+//    }
+//    let mid = (left + right + 1)/2
+//    if mid == right {
+//        return mid
+//    }
+//    if target < nums[mid] {
+//        return index4Insert(nums, target: target, left: left, right: mid)
+//    }else {
+//        return index4Insert(nums, target: target, left: mid, right: right)
+//    }
+//}
 func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-    if target <= nums[0] {
-        return 0
+    var left = 0
+    var right = nums.count - 1
+    while left <= right {
+        let mid = (left + right) / 2
+        if nums[mid] == target {
+            return mid
+        }else if nums[mid] < target {
+            left = mid + 1
+        }else {
+            right = mid - 1
+        }
     }
-  
-    let right = nums.count - 1
-    if target == nums[right] {
-        return right
-    }
-    if target > nums[right] {
-        return right + 1
-    }
-    
-    return index4Insert(nums, target: target, left: 0, right: right)
-}
-
-private func index4Insert(_ nums: [Int], target: Int, left:Int,right:Int) -> Int{
-    
-    if target == nums[right] {
-        return right
-    }
-    if target == nums[left] {
-        return left
-    }
-    let mid = (left + right + 1)/2
-    if mid == right {
-        return mid
-    }
-    if target < nums[mid] {
-        return index4Insert(nums, target: target, left: left, right: mid)
-    }else {
-        return index4Insert(nums, target: target, left: mid, right: right)
-    }
+    return left
 }
