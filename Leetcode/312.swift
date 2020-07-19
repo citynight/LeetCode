8553799d3c930869a294c19c6c64abbd02670a45
lxzzzzzz <lixzzzzzz@126.com>
2020-07-19 09:46:30 +0800

    优化

--- Leetcode/312.swift
+++ Leetcode/312.swift
@@ -12,14 +12,14 @@ class _312: NSObject {
 
 }
 
-func maxCoins(_ nums: [Int]) -> Int {
+func maxCoins1(_ nums: [Int]) -> Int {
     var ans = 0
     var origan = nums
-    helper(&origan, coins: 0, ans: &ans)
+    helper1(&origan, coins: 0, ans: &ans)
     return ans
 }
 
-private func helper(_ nums:inout Array<Int>, coins:Int, ans:inout Int) {
+private func helper1(_ nums:inout Array<Int>, coins:Int, ans:inout Int) {
     // boundary 边界条件
     if nums.count == 0 {
         ans = max(ans, coins)
@@ -30,8 +30,35 @@ private func helper(_ nums:inout Array<Int>, coins:Int, ans:inout Int) {
         let tmp = nums[i]
         let delta = nums[i] * ( i - 1 < 0 ? 1 : nums[i-1]) * ( i + 1 > nums.count - 1 ? 1 : nums[i + 1])
         nums.remove(at: i)
-        helper(&nums, coins: coins + delta, ans: &ans)
+        helper1(&nums, coins: coins + delta, ans: &ans)
         // 回溯
         nums.insert(tmp, at: i)
     }
+}
+
+private var dp:[[Int]]!
+func maxCoins(_ nums: [Int]) -> Int {
+    let len = nums.count
+    var origan = nums
+    // 前面添加1后面添加1
+    origan.insert(1, at: 0)
+    origan.append(1)
+    
+    dp = Array<[Int]>(repeating: Array<Int>(repeating: 0, count: len + 2), count: len + 2)
+    return helper(origan, i: 1, j: len)
+}
+
+func helper(_ nums: [Int], i: Int,j: Int) -> Int {
+    if i > j { return 0 }
+    if dp[i][j] > 0 {
+        return dp[i][j]
+    }
+    // search
+    for k in i...j {
+        let left = helper(nums, i: i, j: k - 1)
+        let right = helper(nums, i: k + 1, j: j)
+        let delta = nums[k] * nums[i - 1] * nums[j + 1]
+        dp[i][j] = max(dp[i][j], left + right + delta)
+    }
+    return dp[i][j]
 }
